359e8624350fbf9174c524e75f86073d9d5307e1
mkolurunaganna <mkolurunaganna@synamedia.com>
2025-05-24 14:12:41 +0530

    Splash screen, Main screen UI design & Data classes for json

--- app/src/main/java/com/example/weatherapp/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp/SplashActivity.kt
@@ -1,21 +1,29 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    //0cfc28cfb8ff5b2a115c98629ae6871f
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000) // 3 seconds delay
     }
 }
