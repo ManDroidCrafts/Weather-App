880d7cc53e9a3582695e7cdd13ab355a7e9164f2
mkolurunaganna <mkolurunaganna@synamedia.com>
2025-05-25 21:00:28 +0530

    Functionality and UI updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.weatherapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,6 +48,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
     implementation(libs.lottie)
 }
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,21 +1,148 @@
 package com.example.weatherapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     //0cfc28cfb8ff5b2a115c98629ae6871f
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        fetchWeatherData("Bengaluru")
+        searchCity()
+    }
+
+    private fun searchCity() {
+        val searchview = binding.searchView
+        searchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    Log.d("Search", "onQueryTextSubmit: $query")
+                    fetchWeatherData(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+    }
+
+    private fun fetchWeatherData(cityName : String) {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+
+        val response = retrofit.getWeatherData(cityName,"0cfc28cfb8ff5b2a115c98629ae6871f", "metric")
+        response.enqueue(object : Callback<WeatherApp> {
+            override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
+                val responseBody = response.body()
+                if (response.isSuccessful && responseBody != null) {
+                    val temperatute = responseBody.main.temp.toString()
+                    val humidity = responseBody.main.humidity.toString()
+                    val windSpeed = responseBody.wind.speed.toString()
+                    val sunrise = responseBody.sys.sunrise.toLong()
+                    val sunset = responseBody.sys.sunset.toLong()
+                    val maxTemp = responseBody.main.temp_max.toString()
+                    val minTemp = responseBody.main.temp_min.toString()
+                    val seaLevel = responseBody.main.sea_level.toString()
+                    val conditions = responseBody.weather.firstOrNull()?.main ?: "Unknown"
+
+                    binding.let {
+                        it.currentTemp.text = "$temperatute °C"
+                        it.currentWeatherCondition.text = conditions
+                        it.maxTemp.text = "Max $maxTemp °C"
+                        it.minTemp.text = "Min $minTemp °C"
+                        it.humidityValue.text = "$humidity %"
+                        it.windValue.text = "$windSpeed m/s"
+                        it.currentWeatherValue.text = conditions
+                        it.sunriseValue.text = "${time(sunrise)}"
+                        it.sunsetValue.text = "${time(sunset)}"
+                        it.seaValue.text = "$seaLevel hPa"
+                        it.day.text = dayName()
+                        it.date.text = currentDate()
+                        it.location.text = cityName
+                    }
+                    changeImageAccordingToWeather(conditions)
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun changeImageAccordingToWeather(conditions: String) {
+        when(conditions) {
+            "Clear Sky", "Sunny", "Clear" -> {
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
+            "Partly Clouds", "Clouds", "Overcast", "Mist", "Foggy" -> {
+                binding.root.setBackgroundResource(R.drawable.colud_background)
+                binding.lottieAnimationView.setAnimation(R.raw.cloud)
+            }
+            "Light Rain", "Dizzle", "Moderate Rain", "Showers", "Heavy Rain", "Rain" -> {
+                binding.root.setBackgroundResource(R.drawable.rain_background)
+                binding.lottieAnimationView.setAnimation(R.raw.rain)
+            }
+            "Light Snow", "Moderate Snow", "Heavy Snow", "Blizzard" -> {
+                binding.root.setBackgroundResource(R.drawable.snow_background)
+                binding.lottieAnimationView.setAnimation(R.raw.snow)
+            }
+            else -> {
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
+        }
+        binding.lottieAnimationView.playAnimation()
+
+    }
+
+    private fun currentDate(): String {
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val formattedDate = currentDate.format(formatter)
+        return  formattedDate
+    }
+
+    private fun time(timeStamp : Long): String {
+        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+        return sdf.format(Date(timeStamp * 1000))
+    }
+
+    fun dayName() : String {
+        val sdf = SimpleDateFormat("EEEE", Locale.getDefault())
+        return sdf.format((Date()))
     }
 }
